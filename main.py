22f87f27b088e83e6e873e915299f9f28314f564
AnimeVerse972 <soibjonovdiyorbek0@gmail.com>
2025-08-05 17:24:23 +0500

    Update main.py

--- main.py
+++ main.py
@@ -598,25 +598,38 @@ async def add_kino_handler(message: types.Message, state: FSMContext):
         if len(parts) < 5:
             failed += 1
             continue
+
         code, server_channel, reklama_id, post_count = parts[:4]
         title = " ".join(parts[4:])
+
         if not (code.isdigit() and reklama_id.isdigit() and post_count.isdigit()):
             failed += 1
             continue
+
         reklama_id = int(reklama_id)
         post_count = int(post_count)
 
-        # Faqat ma'lumotlarni bazaga qo'shamiz
         await add_kino_code(code, server_channel, reklama_id + 1, post_count, title)
-        successful += 1  # muvaffaqiyatli bo'lganlar hisoblanmoqda
 
-    await message.answer(f"""✅ Yangi kodlar qo‘shildi:
-✅ Muvaffaqiyatli: {successful}
-❌ Xatolik: {failed}""")
+        download_btn = InlineKeyboardMarkup().add(
+            InlineKeyboardButton("📥 Yuklab olish", url=f"https://t.me/{BOT_USERNAME}?start={code}")
+        )
 
+        try:
+            for ch in MAIN_CHANNELS:
+                await bot.copy_message(
+                    chat_id=ch,
+                    from_chat_id=server_channel,
+                        message_id=reklama_id,
+                reply_markup=download_btn
+        ) 
+
+            successful += 1
+        except:
+            failed += 1
+
+    await message.answer(f"✅ Yangi kodlar qo‘shildi:\n\n✅ Muvaffaqiyatli: {successful}\n❌ Xatolik: {failed}")
     await state.finish()
-
-
 
 # === Kodlar ro‘yxati
 @dp.message_handler(lambda m: m.text.strip() == "📄 Kodlar ro‘yxati")
